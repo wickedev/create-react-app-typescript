41d4046dadf5762efdeecd112317146cbcd16cdb
wickedev <orange881217@gmail.com>
2018-06-13 20:32:38 +0900

    dependencies added

--- packages/react-scripts/template/src/App/index.tsx
+++ packages/react-scripts/template/src/App/index.tsx
@@ -1,8 +1,23 @@
+import 'App/styles.scss'
+import logo from 'assets/logo.svg'
+import {observable} from 'mobx'
+import {observer} from 'mobx-react'
 import * as React from 'react'
-import 'src/App/styles.scss'
-import logo from 'src/assets/logo.svg'
 
-class App extends React.Component {
+export interface IAppProps {
+    initialCount: number
+}
+
+@observer
+class App extends React.Component<IAppProps> {
+
+    @observable private count: number
+
+    constructor(props: IAppProps) {
+        super(props)
+        this.count = props.initialCount
+    }
+
     public render() {
         return (
             <div className="App">
@@ -13,8 +28,23 @@ class App extends React.Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.tsx</code> and save to reload.
                 </p>
+                <div>
+                    <button onClick={this.onClickMinus}>-</button>
+                    {` ${this.count} `}
+                    <button onClick={this.onClickPlus}>+</button>
+                </div>
             </div>
         )
+    }
+
+    private onClickMinus = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        this.count--
+    }
+
+    private onClickPlus = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        this.count++
     }
 }
 
